d5cb376e602c25052d5a4331c446121efa87bca2
Brandon Dube <brandon@retrorefractions.com>
2024-11-29 08:00:02 -0800

    IERS Bulletin C 68

--- tai.go
+++ tai.go
@@ -45,14 +45,14 @@ const (
 var (
 	// LastKnownBulletinCUpdate is the last known issue of Bulletin C by the
 	// IERS that pkg tai was updated for
-	LastKnownBulletinCUpdate = 67
+	LastKnownBulletinCUpdate = 68
 	// LastKnownBulletinCTime is the date on which the last known Bulletin C
 	// was released
-	LastKnownBulletinCTimestamp = Gregorian{Year: 2024, Month: January, Day: 8}
+	LastKnownBulletinCTimestamp = Gregorian{Year: 2024, Month: July, Day: 4}
 
 	// PkgUpToDateUntil is the moment in time at which the last known bulletin C
 	// update is made invalid
-	PkgUpToDateUntil = Gregorian{Year: 2024, Month: July, Day: 1}
+	PkgUpToDateUntil = Gregorian{Year: 2025, Month: January, Day: 1}
 
 	leaps = []leap{
 		{63100800, 10},
